ea8ba4fb8239fd52decbcbf6a1e16269aa3d9aaa
Rosano Coutinho <pub@xeari.com>
2020-12-11 18:18:30 -0300

    Spec keyboard + accessible

--- ui-test-_misc.js
+++ ui-test-_misc.js
@@ -30,8 +30,10 @@ describe('OLSKStandardFeatures_Misc', function () {
 				browser.assert.attribute(`${ OLSKStandardFeaturesItem }:nth-child(${ i + 1 }) ${ OLSKStandardFeaturesItemIdentity }`, 'src', [
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureSync.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureOffline.svg',
-						'/node_modules/OLSKUIAssets/_OLSKSharedFeaturePrivate.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureEverywhere.svg',
+						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureKeyboard.svg',
+						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureAccessible.svg',
+						'/node_modules/OLSKUIAssets/_OLSKSharedFeaturePrivate.svg',
 					][i]);
 			});
 			
