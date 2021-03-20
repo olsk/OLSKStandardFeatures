96033cc0ff389e737a90fabf8c6aaedcbfe4f290
Rosano Coutinho <pub@xeari.com>
2021-03-20 10:29:50 -0300

    Extract transport feature

--- ui-test-_misc.js
+++ ui-test-_misc.js
@@ -20,6 +20,7 @@ describe('OLSKAppFeatureList_Misc', function () {
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureOffline.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureEverywhere.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureKeyboard.svg',
+						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureTransport.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureAccessible.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeaturePrivate.svg',
 						'/node_modules/OLSKUIAssets/_OLSKSharedFeatureZeroData.svg',
